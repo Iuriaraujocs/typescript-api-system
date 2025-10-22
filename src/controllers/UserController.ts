54a168a6a995ced08f94eb0b17d267cb4869e7a1
Iuri Araujo <iuriaraujoc.eng@gmail.com>
2025-10-22 09:05:41 -0300

    removing services garbage

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,10 @@
 import { Request, Response } from "express";
-// import { CreateUserService } from "../services/CreateUserService";
 import { UserRepository } from "../repositories/UserRepository";
 
 export class UserController {
   async create(req: Request, res: Response) {
     try {
       const { name, email } = req.body;
-    //   const service = new CreateUserService();
-    //   const user = await service.execute({ name, email });
       const user = UserRepository.create({ name, email });
       await UserRepository.save(user);
       return res.status(201).json(user);
