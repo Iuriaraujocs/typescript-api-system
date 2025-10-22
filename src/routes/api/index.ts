3b1190f2a3a0b6d819bd7b1b8cb50029ad04361a
Iuri Araujo <iuriaraujoc.eng@gmail.com>
2025-10-22 18:24:09 -0300

    removed unecessary comments

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,14 +1,9 @@
 import { Router } from 'express';
-// import { authMiddleware } from '../../middlewares/auth.middleware';
 import userRoutes from './user.routes';
 
 const apiRouter = Router();
 
-// Middlewares específicos para rotas de API (ex: autenticação, logs, etc.)
-// apiRouter.use(authMiddleware);
-
 // Rotas internas da API
 apiRouter.use('/users', userRoutes);
-// apiRouter.use('/something', somethingRoutes);
 
 export default apiRouter;
